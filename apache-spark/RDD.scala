7f5e46a96f1e6126b933fa93586c478a7138787e
Man Njoro <elijohnmwoho@gmail.com>
2024-11-21 22:33:25 +0300

    "Added code to create RDD from text file, perform word count, and print first and top 5 results."

--- apache-spark/RDD.scala
+++ apache-spark/RDD.scala
@@ -4,4 +4,8 @@ object RDD extends App {
     val sc = new SparkContext(conf)
     val rdd = sc.parallelize(Array(1,2,3,4,5,6,7,8))
     rdd.foreach(println)
+    val rdd = sc.textFile("path")
+    val wc = rdd.flatMap(line => line.split(",")).map(word => (word, 1)).reduceByKey(_+_)
+    wc.first()
+    wc.take(5)
 }
