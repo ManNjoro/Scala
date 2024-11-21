311e1ecad30d18def774589309314ff0a22ad9d5
Man Njoro <elijohnmwoho@gmail.com>
2024-11-21 22:48:30 +0300

    "Added SparkSession and created an RDD from a sequence of strings, in addition to existing RDD creation from an array and a text file."

--- apache-spark/RDD.scala
+++ apache-spark/RDD.scala
@@ -1,7 +1,10 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
 object RDD extends App {
     val conf = new SparkConf().setAppName("SimpleRDDApp").setMaster("local[1]")
     val sc = new SparkContext(conf)
+    val spark = SparkSession.builder().master("local[1]").appName("myApp").getOrCreate()
+    val rdd1 = spark.sparkContext.parallelize(Seq("the", "cow", "has"))
     val rdd = sc.parallelize(Array(1,2,3,4,5,6,7,8))
     rdd.foreach(println)
     val rdd = sc.textFile("path")
