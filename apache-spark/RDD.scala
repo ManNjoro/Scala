4386cef02886d5de173c2cb0849efdd9c31f5e98
Man Njoro <elijohnmwoho@gmail.com>
2024-11-26 23:09:57 +0300

    StructType and StructFields

--- apache-spark/RDD.scala
+++ apache-spark/RDD.scala
@@ -11,4 +11,29 @@ object RDD extends App {
     val wc = rdd.flatMap(line => line.split(",")).map(word => (word, 1)).reduceByKey(_+_)
     wc.first()
     wc.take(5)
+
+    // Using StructType & StructField
+    import org.apache.spark.sql.types._
+    import org.apache.spark.sql.Row
+    val mySchema = StructType(Array(
+        StructField("Name",StringType,true),
+        StructField("Position",StringType,true),
+        StructField("Gender", StringType, true),
+        StructField("Age",IntegerType,true),
+        ))
+    val myData=Seq(Row("John","Manager","Male",38),
+            Row("Mary", "Director","Female", 45),
+            Row("Sally", "Engineer","Female", 30),
+            Row("Okelo", "Clerk","Male", 30),
+            Row("Wiclife", "Trainers","Male", 60),
+            Row("Wallace", "Clerk","Male", 40),
+            Row("Eunice", "Trainer","Female", 35),
+            Row("Barack", "Trainer","Male", 30),
+            Row("Simon", "Trainer", "Male", 55)
+            )
+    val mydf = spark.createDataFrame(spark.sparkContext.parallelize(myData),mySchema)
+    val bksds2 =
+    bksds.agg(sum("Stock"),max("Stock"),
+    min("Yopb")).show()
+    
 }
